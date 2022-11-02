ad76f3135ad6ab08eb525df8a915486cea0b37b7
ShahadAloufi <shahdse001@gmail.com>
2022-11-02 10:23:38 +0300

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by shahadmufleh on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
